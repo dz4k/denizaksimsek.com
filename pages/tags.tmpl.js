0656dc401fb7595723e19d23b7b1e92b1e6ce9ce
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-03-24 16:23:56 +0300

    tag pages

---
+++ pages/tags.tmpl.js
@@ -0,0 +1,15 @@
+
+export default function* ({ intl, comp, search }) {
+    for (const lang of ["tr", "en", "tok"]) {
+        for (const tag of ["blog", "project", "link"]) {
+            yield {
+                url: `/tag/${lang}/${tag}/`,
+                layout: "layouts/collection.eta",
+                lang,
+                tags: "collection",
+                title: intl["Tag"][lang] + ": " + intl[tag + " (tag plural)"][lang] + " " + intl["(lang specifier)"][lang],
+                content: comp.entryList({ entries: search.pages(`${tag} lang=${lang}`, "date=desc"), dates: tag !== "project" })
+            }
+        }
+    }
+}
