2ec899b12ea5515a39858f1e28c34ec50e1e8361
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-04-14 21:34:00 +0300

    extract highlighting config

--- _config.ts
+++ _config.ts
@@ -10,16 +10,11 @@ import basePath from "https://deno.land/x/lume@v1.7.1/plugins/base_path.ts";
 import resolveUrls from "https://deno.land/x/lume@v1.7.1/plugins/resolve_urls.ts";
 import eta from "https://deno.land/x/lume@v1.7.1/plugins/eta.ts";
 
-import prismHighlight from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/prism/mod.ts"
-import Prism from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/prism/deps.ts"
-import prismHyperscript from "https://unpkg.com/prism-hyperscript@1.1.0/prism-hyperscript.mjs"
-
+import highlighting from "./_build/highlighting.ts"
 import getDatesFromGit from "./_build/get-dates-from-git.ts"
 import backlinks from "./_build/backlinks.ts"
 import prose, { markdownOptions } from "./_build/prose.ts"
 import myFilters from "./_build/filters.ts"
-
-prismHyperscript(Prism)
 
 export default lume(
     { location: new URL("https://denizaksimsek.com/") },
@@ -31,7 +26,7 @@ export default lume(
   .data("lang", "en")
 
   .use(date({ locales: { tr, en_US } }))
-  .use(prismHighlight())
+  .use(highlighting())
   .use(basePath())
   .use(resolveUrls())
   .use(eta())
