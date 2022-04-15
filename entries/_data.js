908810f64aec9c9a46b6b8bbaaf22dc6eeafdbcd
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-04-15 15:52:06 +0300

    minor

--- entries/_data.js
+++ entries/_data.js
@@ -2,7 +2,7 @@ export const layout = "layouts/entry.eta"
 export const templateEngine = "eta,md"
 export const renderOrder = -10
 export const url = page => {
-    const [_empty, _entriesDir, ...rest] = page.src.path.split("/")
+    const [, _entriesDir, ...rest] = page.src.path.split("/")
     if (rest[0] === "blog") rest.shift()
     const href = "/" + rest.join("/") + "/"
     return href
