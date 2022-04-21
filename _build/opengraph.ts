2407d97b63a097b2c0a6d46aa4735c8c4b1e9448
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-04-21 20:20:29 +0300

    add opengraph embeds

---
+++ _build/opengraph.ts
@@ -0,0 +1,28 @@
+
+import { DOMParser, DOMParserMimeType } from "lume/deps/dom.ts"
+
+export async function parse(href: string) {
+    const res = await fetch(href, { headers: { "Expect": "text/html" } })
+    const html = await res.text()
+    
+    const contentType = res.headers.get("Content-Type")?.split(";")[0] as DOMParserMimeType ?? "text/html"
+    const document = new DOMParser().parseFromString(html, contentType)!
+
+    const title = (
+        document.querySelector("meta[property='og:title']")?.getAttribute("content")
+        ?? document.querySelector("title")?.textContent
+    )
+
+    const description = (
+        document.querySelector("meta[property='og:description']")?.getAttribute("content")
+        ?? document.querySelector("meta[name='description']")?.getAttribute("content")
+    )
+
+    const content = (
+        document.querySelector(".e-content")
+        ?? document.querySelector("content")
+        ?? document.querySelector("p")
+    )?.textContent
+
+    return { title, description, content }
+}
--- _config.ts
+++ _config.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck lume config
 
 import "https://deno.land/x/dotenv@v3.2.0/load.ts"
 
