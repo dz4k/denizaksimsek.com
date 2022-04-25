468419760983c2149681444f035939010ae33aa2
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-04-25 06:46:02 +0300

    esm.sh

--- _build/highlighting.ts
+++ _build/highlighting.ts
@@ -1,7 +1,7 @@
 
 import prismHighlight from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/prism/mod.ts"
 import Prism from "https://raw.githubusercontent.com/lumeland/experimental-plugins/main/prism/deps.ts"
-import prismHyperscript from "https://unpkg.com/prism-hyperscript@1.1.0/prism-hyperscript.mjs"
+import prismHyperscript from "https://esm.sh/prism-hyperscript"
 import type { Site } from "lume/core.ts"
 
 export default () => {
