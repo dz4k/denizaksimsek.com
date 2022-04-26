a08e96c14578e6c673d5db4ac375af398891f9e7
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-04-26 10:50:55 +0300

    format config

--- _config.ts
+++ _config.ts
@@ -1,20 +1,21 @@
 
 import "https://deno.land/x/dotenv@v3.2.0/load.ts"
 
-import lume from "lume/mod.ts"
-import date from "lume/plugins/date.ts"
+import lume        from "lume/mod.ts"
+import date        from "lume/plugins/date.ts"
+import basePath    from "lume/plugins/base_path.ts"
+import resolveUrls from "lume/plugins/resolve_urls.ts"
+import eta         from "lume/plugins/eta.ts"
+import bundler     from "lume/plugins/bundler.ts"
+
 import tr    from "https://deno.land/x/date_fns@v2.22.1/locale/tr/index.js"
 import en_US from "https://deno.land/x/date_fns@v2.22.1/locale/en-US/index.js"
-import basePath from "lume/plugins/base_path.ts"
-import resolveUrls from "lume/plugins/resolve_urls.ts"
-import eta from "lume/plugins/eta.ts"
-import bundler from "lume/plugins/bundler.ts"
 
-import highlighting from "./_build/highlighting.ts"
-import getDatesFromGit from "./_build/get-dates-from-git.ts"
-import backlinks from "./_build/backlinks.ts"
+import highlighting               from "./_build/highlighting.ts"
+import getDatesFromGit            from "./_build/get-dates-from-git.ts"
+import backlinks                  from "./_build/backlinks.ts"
 import prose, { markdownOptions } from "./_build/prose.ts"
-import myFilters from "./_build/filters.ts"
+import myFilters                  from "./_build/filters.ts"
 
 export default lume(
     { location: new URL("https://denizaksimsek.com/") },
