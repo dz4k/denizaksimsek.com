b4010ae80f45ae128d342994e3e58becf25cc2ac
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-04-27 15:58:43 +0300

    fix

--- entries/_data.js
+++ entries/_data.js
@@ -1,9 +1,11 @@
-export const layout = "layouts/entry.eta"
-export const templateEngine = "eta,md"
-export const renderOrder = -10
-export const url = page => {
-    const [, _entriesDir, ...rest] = page.src.path.split("/")
-    if (rest[0] === "blog") rest.shift()
-    const href = "/" + rest.join("/") + "/"
-    return href
-}
+export const
+    layout = "layouts/entry.eta",
+    templateEngine = "eta,md",
+    renderOrder = -10,
+    content = "",
+    url = page => {
+        const [, _entriesDir, ...rest] = page.src.path.split("/")
+        if (rest[0] === "blog") rest.shift()
+        const href = "/" + rest.join("/") + "/"
+        return href
+    }
