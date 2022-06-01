4f4b33a89b121eada8a365489daa929c5600d5c1
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-06-02 00:26:42 +0300

    add asciimath page

--- _config.ts
+++ _config.ts
@@ -6,7 +6,6 @@ import date        from "lume/plugins/date.ts"
 import basePath    from "lume/plugins/base_path.ts"
 import resolveUrls from "lume/plugins/resolve_urls.ts"
 import eta         from "lume/plugins/eta.ts"
-import bundler     from "lume/plugins/bundler.ts"
 
 import tr    from "https://deno.land/x/date_fns@v2.22.1/locale/tr/index.js"
 import en_US from "https://deno.land/x/date_fns@v2.22.1/locale/en-US/index.js"
@@ -24,6 +23,7 @@ export default lume(
   
   .ignore("README.md")
   .copy("assets")
+  .copy("client")
   .data("lang", "en")
 
   .use(date({ locales: { tr, en_US } }))
---
+++ client/compose.js
@@ -0,0 +1,30 @@
+
+function post(filename, folder, body) {
+    open(
+        `https://github.com/dz4k/denizaksimsek.com/`+
+        `new/master/entries/${folder}/new`+
+        `?filename=${filename}`+
+        `&value=${body}`
+    )
+}
+
+function blogPost(body) {
+    const date = new Date
+    const folder = `blog/${date.getFullYear()}`
+    const filename = date.toISOString() + ".md"
+    const body = $content.value
+
+    post(filename, folder, body)
+}
+
+async function bookmarkPost(url, content) {
+    const folder = "interact"
+    const filename = new Date().toISOString() + ".json"
+    const body = JSON.stringify({
+        "bookmark of": {
+            name: await getTitle(url),
+            url,
+        },
+        content,
+    })
+}
