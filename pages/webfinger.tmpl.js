86ec53fb03443e50f002b79829a7e43154fd149f
Deniz Akşimşek <deniz@denizaksimsek.com>
2023-04-24 21:29:06 +0300

    webfinger take 2

--- pages/webfinger.tmpl.js
+++ pages/webfinger.tmpl.js
@@ -1,26 +1,9 @@
-export const url = "/.well-known/webfinger";
-export default () => JSON.stringify({
-    "subject": "acct:dz4k@indieweb.social",
-    "aliases":
-    [
-        "https://indieweb.social/@dz4k",
-        "https://indieweb.social/users/dz4k"
-    ],
-    "links":
-    [
-        {
-            "rel":"http://webfinger.net/rel/profile-page",
-            "type":"text/html",
-            "href":"https://indieweb.social/@dz4k"
-        },
-        {
-            "rel":"self",
-            "type":"application/activity+json",
-            "href":"https://indieweb.social/users/dz4k"
-        },
-        {
-            "rel":"http://ostatus.org/schema/1.0/subscribe",
-            "template":"https://indieweb.social/authorize_interaction?uri={uri}"
-        }
-    ]
-}, null, 2)
+export const url = '/.well-known/webfinger';
+export default async () =>
+	JSON.stringify(
+		await fetch(
+			'https://indieweb.social/.well-known/webfinger?resource=acct:dz4k@indieweb.social',
+		).then((res) => res.json()),
+		null,
+		2,
+	);
