87a8c6782a0b0953e4b4aa5316ab65cbf63d6db0
Deniz Akşimşek <deniz@denizaksimsek.com>
2023-04-24 21:36:54 +0300

    webfinger take 3

--- pages/webfinger.tmpl.js
+++
@@ -1,9 +0,0 @@
-export const url = '/.well-known/webfinger';
-export default async () =>
-	JSON.stringify(
-		await fetch(
-			'https://indieweb.social/.well-known/webfinger?resource=acct:dz4k@indieweb.social',
-		).then((res) => res.json()),
-		null,
-		2,
-	);
